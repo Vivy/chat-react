c36fc28c08da34e9229cf4dc6a43662824380f93
Vivy <Tomitaviorel@yahoo.com>
2023-02-14 17:45:23 +0200

    feat: finished with message and input and css

--- src/component/input/input.jsx
+++ src/component/input/input.jsx
@@ -1,5 +1,20 @@
+import { GrAttachment } from 'react-icons/gr';
+import { SlPicture } from 'react-icons/sl';
+
 const Input = () => {
-  return <div className='input'>Input</div>;
+  return (
+    <div className='input'>
+      <input type='text' placeholder='Text the message here' id='' />
+      <div className='send'>
+        <GrAttachment />
+        <input type='file' style={{ display: 'none' }} id='send-file' />
+        <label htmlFor='send-file'>
+          <SlPicture />
+        </label>
+        <button>Send</button>
+      </div>
+    </div>
+  );
 };
 
 export default Input;
--- src/component/message/message.jsx
+++ src/component/message/message.jsx
@@ -1,5 +1,22 @@
 const Message = () => {
-  return <div>Message</div>;
+  return (
+    <div className='message owner'>
+      <div className='messageInfo'>
+        <img
+          src='https://images.pexels.com/photos/5044316/pexels-photo-5044316.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load'
+          alt=''
+        />
+        <span>the time</span>
+      </div>
+      <div className='messageContent'>
+        <p>the message</p>
+        {/* <img
+          src='https://images.pexels.com/photos/5044316/pexels-photo-5044316.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load'
+          alt=''
+        /> */}
+      </div>
+    </div>
+  );
 };
 
 export default Message;
